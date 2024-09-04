1d5fc63aa7d62889c34d90984534bfc3fda2ab22
Vinicius Domingos <viniciusda95@gmail.com>
2024-09-03 21:28:26 -0300

    funcionando
    a

--- main.py
+++ main.py
@@ -39,9 +39,8 @@ async def on_ready():
 
     # Configura o agendador para a geração automática do Excel
     scheduler = AsyncIOScheduler()
-    trigger_time = datetime.now(timezone.utc) + timedelta(minutes=2)
     scheduler.add_job(gerar_excel_automatico, CronTrigger(start_date=trigger_time))
-    #scheduler.add_job(gerar_excel_automatico, CronTrigger(day_of_week='mon-fri', hour=21, minute=21))
+    scheduler.add_job(gerar_excel_automatico, CronTrigger(day_of_week='mon-fri', hour=21, minute=21))
     print("Scheduler configurado!")
     scheduler.start()
     print("Scheduler iniciado!")
